edecf0713ca2bc386ed81c5356b0659e3b8608f7
sgfrdgrln <gazel0123@gmail.com>
2024-03-05 20:23:48 +0800

    first commit

---
+++ RunEmployee.java
@@ -0,0 +1,30 @@
+// Instruction
+
+// 1. Create four (4) Java classes. Name them: RunEmployee, Employee,
+// FullTimeEmployee, PartTimeEmployee. The RunEmployee shall contain the main method
+// and will be used to execute the program.
+
+// 2. Write a simple payroll program that will display employee's information.
+// Refer to UML Class Diagram for the names of the variable and method. This should
+// be the sequence of the program upon execution:
+
+// a. Ask the user to input the name of the employee
+
+// b. Prompt the user to select between full time and part time by pressing
+// either F (full time) or P (part time).
+
+// c. If F is pressed, ask the user to type his monthly salary. Then, display his 
+// name and monthly salary.
+
+// If P is pressed, ask the user to type his rate (pay) per hour and the number of
+// hours he worked for the entire month seperated by a space. Then, display name
+// and wage.
+
+// Note: You can add variables and methods if needed. Just make sure that all the 
+// variables and methods given in the diagram are properly used.
+
+// Employee class should contain a String name, a getter and setter method setName(): void, getName(): String
+// FullTimeEmployee class should contain double monthlySalary, a getter and setter method
+// setMonthlySalary(): void, getMonthlySalary(): double
+// PartTimeEmployee class should contain double ratePerHour, int hoursWorked, double wage, a getter and setter
+// method setWage(): void, getWage() : double
