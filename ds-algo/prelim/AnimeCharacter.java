e091fc165df0bbf50b5d0dd822d6ede98e7af131
sgfrdgrln <gazel0123@gmail.com>
2024-10-02 21:01:20 +0800

    added some school activity

---
+++ ds-algo/prelim/AnimeCharacter.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class AnimeCharacter {
+    
+    public static void main(String[] args) {
+
+       
+        
+        LinkedList<String> animeCharacter = new LinkedList<>();
+
+        animeCharacter.add("Yato");
+        animeCharacter.add("Killua");
+        animeCharacter.add("Saitama");
+        animeCharacter.add("Yami");
+        System.out.print("Anime characters: \n" + animeCharacter);
+       
+
+
+    }
+
+}
